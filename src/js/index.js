e51148b75c81c0e1dd7572d02e96aee2b33f0ab5
heyz-scott <jianyong.zhou@heyz.com>
2016-10-27 17:30:04 +0800

    Html5 online editor

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,11 @@ import VueSelectCity from "./vue/selectCity.vue"
 import VueDatePicker from "./vue/datePicker.vue"
 import VueBeautyAlert from "./vue/beautyAlert.vue"
 import VueCrop from "./vue/crop.vue"
+import VueHtmlEditor from "./vue/htmlEditor.vue"
 
 new Vue( {
     el: "#app",
-    components: { VueSelectCity, VueDatePicker, VueBeautyAlert, VueCrop },
+    components: { VueSelectCity, VueDatePicker, VueBeautyAlert, VueCrop, VueHtmlEditor },
     methods: {
         alertOne () {
             this.$refs.alert.show('这是提示内容')
