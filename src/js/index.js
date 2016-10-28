82c21960954416370240ac40645d1e90364810b9
heyz-scott <jianyong.zhou@heyz.com>
2016-10-28 16:33:33 +0800

    Pure Select

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,20 @@ import VueDatePicker from "./vue/datePicker.vue"
 import VueBeautyAlert from "./vue/beautyAlert.vue"
 import VueCrop from "./vue/crop.vue"
 import VueHtmlEditor from "./vue/htmlEditor.vue"
+import VuePureSelect from "./vue/pureSelect.vue"
 
 new Vue( {
     el: "#app",
-    components: { VueSelectCity, VueDatePicker, VueBeautyAlert, VueCrop, VueHtmlEditor },
+    data: {
+        diploma: {
+            chuzhong: "初中",
+            gaozhong: "高中",
+            benke: "本科",
+            shuoshi: "硕士",
+            boshi: "博士"
+        }
+    },
+    components: { VueSelectCity, VueDatePicker, VueBeautyAlert, VueCrop, VueHtmlEditor, VuePureSelect },
     methods: {
         alertOne () {
             this.$refs.alert.show('这是提示内容')
---
+++ src/js/vue/pureSelect.vue
@@ -0,0 +1,27 @@
+<template>
+    <dl class="vue-pure-select">
+        <dt>
+            <template v-for="(elem, key) of elems">
+                <input :id="key" type="radio" :name="name" :value="key" v-model="value">
+                <span>{{elem}}</span>
+            </template>
+        </dt>
+        <dd>
+            <label :for="key" v-for="(elem, key) of elems">{{elem}}</label>
+        </dd>
+    </dl>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                value: "benke"
+            }
+        },
+        props: {
+            name: String,
+            elems: Object
+        }
+    }
+</script>
