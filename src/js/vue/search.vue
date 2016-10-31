285cbb1548280fdbe36af0c28c211e69fda71bce
heyz-scott <jianyong.zhou@heyz.com>
2016-10-31 16:38:56 +0800

    Add searching component; beautify datepicker

---
+++ src/js/vue/search.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="vue-search">
+        <div class="icon">
+            <svg>
+                <use xlink:href="public/img/icons.svg#icon-search"></use>
+            </svg>
+        </div>
+        <input class="input" type="text" placeholder="输入关键词">
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
