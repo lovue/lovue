dbbb5bc4ddac3f1fec79ea115117d1ee29744057
loliconer <vuedev@163.com>
2017-06-28 18:45:13 +0800

    Sync code

--- src/js/vue/PwdStrength.vue
+++ src/js/vue/PwdStrength.vue
@@ -1,89 +1,53 @@
 <template>
-  <div class="vue-pwd-strength">
-    <div class="pwd-col">
-      <span class="info-text">密码强度</span>
+  <div class="vue-pwd-strength" v-show="password.length">
+    <div class="str-lines">
+      <span :class="n" v-for="n of levelClasses"></span>
     </div>
-    <div class="pwd-col">
-      <div class="pwd-base" :class="levelClass" v-for="levelClass of levelClasses"></div>
-    </div>
-    <div class="pwd-col">
-      <span :class="`${levelClasses[0]}-text`" v-if="level">{{strengthInfo}}</span>
-    </div>
+    <div class="str-info">{{strengthInfo}}</div>
   </div>
 </template>
 
 <script>
+  let elem = document.createElement('script')
+  elem.src = '/js/vendor/zxcvbn.min.js'
+  elem.async = true
+  document.head.appendChild(elem)
+
   export default{
     data() {
       return {
-        level: 0
+        score: 0
       }
     },
     props: {
       password: {
         type: String,
         default: ''
-      },
-      // 密码最小长度，低于此则为弱密码
-      minLength: {
-        type: Number,
-        default: 6
       }
     },
     watch: {
       password() {
-        this.pwdStrengthCheck()
+        this.score = zxcvbn(this.password).score
       }
     },
     computed: {
       strengthInfo() {
-        let words = ['', '弱', '中', '强']
-        return words[this.level]
+        let words = ['极弱', '弱', '中', '强', '极强']
+        return words[this.score]
       },
       levelClasses() {
         let classes = [
-          ['', '', ''],
-          ['pwd-weak', '', ''],
-          ['pwd-medium', 'pwd-medium', ''],
-          ['pwd-strong', 'pwd-strong', 'pwd-strong']
+          ['str-0', '', '', '', ''],
+          ['str-1', 'str-1', '', '', ''],
+          ['str-2', 'str-2', 'str-2', '', ''],
+          ['str-3', 'str-3', 'str-3', 'str-3', ''],
+          ['str-4', 'str-4', 'str-4', 'str-4', 'str-4']
         ]
-        return classes[this.level]
-      },
-    },
-    methods: {
-      pwdStrengthCheck() {
-        this.level = 0
-        // 空密码不检测
-        if (!this.password) {
-          return
-        }
-
-        if (this.password.length < this.minLength) {
-          this.level = 1
-          return
-        }
-
-        let char = /[a-zA-Z]/
-        let number = /[0-9]/
-        let alpha = /[~!@#$%^&*()_+{}\|:"<>?`\-=\\\[\];\',.\/\ ]/
-
-        // 有字母
-        if (char.test(this.password)) {
-          this.level++
-        }
-        // 有数字
-        if (number.test(this.password)) {
-          this.level++
-        }
-
-        // 有符号
-        if (alpha.test(this.password)) {
-          this.level++
-        }
+        return classes[this.score]
       }
     },
     created() {
-      this.pwdStrengthCheck()
+      this.score = zxcvbn(this.password).score
     }
   }
 </script>
