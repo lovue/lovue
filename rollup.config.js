8c441172ca1229a1dfcc550fbd7b8605ad22d9ce
loliconer <vuedev@163.com>
2018-10-13 01:51:32 +0800

    Build multi format bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,25 +2,36 @@ import vue from 'rollup-plugin-vue'
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
 
-const production = process.env.production
+const production = process.env.production, cjs = process.env.cjs, esm = process.env.esm
 let plugins = [
   vue({
     template: {
       isProduction: true
     }
   })
-], file = 'dist/lovue.js'
+], file = 'dist/lovue.js', format = 'iife'
+
+if (cjs) {
+  format = 'cjs'
+  file = 'dist/lovue.common.js'
+}
+
+if (esm) {
+  format = 'esm'
+  file = 'dist/lovue.esm.js'
+}
 
 if (!production) {
   plugins.push(serve('doc'))
   plugins.push(livereload())
   file = 'doc/js/lovue.js'
+  format = 'iife'
 }
 
 export default {
   input: 'src/index.js',
   output: {
-    format: 'iife',
+    format,
     file,
     globals: {
       vue: 'Vue'
