5863b11070bb14383a32745e0dbd398ed56efe70
loliconer <vuedev@163.com>
2018-11-21 15:40:48 +0800

    nonLink模式下高亮选中项

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -8,20 +8,20 @@
           </div>
           <ul class="m-sub" :style="`height: ${menu.height || 0}px;`" v-show="menu._open">
             <li class="s-item" v-for="child of menu.children">
-              <router-link :class="{focus: child._focus}" :to="child.url" v-if="mode === 'spa'">{{child.name}}</router-link>
+              <router-link :to="child.url" v-if="mode === 'spa'">{{child.name}}</router-link>
               <a :class="{focus: child._focus}" :href="child.url" v-if="mode === 'link'">{{child.name}}</a>
-              <a class="link" v-if="mode === 'nonLink'" @click="$emit('click', child.value || child.url)">{{child.name}}</a>
+              <a :class="{focus: child._focus}" v-if="mode === 'nonLink'" @click="clickItem(child, menu)">{{child.name}}</a>
             </li>
           </ul>
         </template>
         <template v-else>
-          <router-link class="i-link" :class="{focus: menu._focus}" :to="menu.url" v-if="mode === 'spa'">
+          <router-link class="i-link" :to="menu.url" v-if="mode === 'spa'">
             <v-icon :icon="menu.icon" size="16" v-if="menu.icon"></v-icon>{{menu.name}}
           </router-link>
           <a class="i-link" :class="{focus: menu._focus}" :href="menu.url" v-if="mode === 'link'">
             <v-icon :icon="menu.icon" size="16" v-if="menu.icon"></v-icon>{{menu.name}}
           </a>
-          <a class="i-link" :class="{focus: menu._focus}" v-if="mode === 'nonLink'" @click="$emit('click', menu.value || menu.url)">
+          <a class="i-link" :class="{focus: menu._focus}" v-if="mode === 'nonLink'" @click="clickItem(menu)">
             <v-icon :icon="menu.icon" size="16" v-if="menu.icon"></v-icon>{{menu.name}}
           </a>
         </template>
@@ -33,24 +33,24 @@
           </div>
           <div class="v-dropdown">
             <template v-if="mode === 'spa'">
-              <router-link class="d-item" :class="{focus: child._focus}" :to="child.url" v-for="(child, i) of menu.children" :key="i">{{child.name}}</router-link>
+              <router-link class="d-item" :to="child.url" v-for="(child, i) of menu.children" :key="i">{{child.name}}</router-link>
             </template>
             <template v-if="mode === 'link'">
               <a class="d-item" :class="{focus: child._focus}" :href="child.url" v-for="child of menu.children">{{child.name}}</a>
             </template>
             <template v-if="mode === 'nonLink'">
-              <a class="d-item" @click="$emit('click', child.value || child.url)" v-for="child of menu.children">{{child.name}}</a>
+              <a class="d-item" @click="clickItem(child, menu)" v-for="child of menu.children">{{child.name}}</a>
             </template>
           </div>
         </div>
         <template v-else>
-          <router-link class="i-link" :class="{focus: menu._focus}" :to="menu.url" v-if="mode === 'spa'">
+          <router-link class="i-link" :to="menu.url" v-if="mode === 'spa'">
             <v-icon :icon="menu.icon" size="16" v-if="menu.icon"></v-icon>{{menu.name}}
           </router-link>
           <a class="i-link" :class="{focus: menu._focus}" :href="menu.url" v-if="mode === 'link'">
             <v-icon :icon="menu.icon" size="16" v-if="menu.icon"></v-icon>{{menu.name}}
           </a>
-          <a class="i-link" :class="{focus: menu._focus}" v-if="mode === 'nonLink'" @click="$emit('click', menu.value || menu.url)">
+          <a class="i-link" :class="{focus: menu._focus}" v-if="mode === 'nonLink'" @click="clickItem(menu)">
             <v-icon :icon="menu.icon" size="16" v-if="menu.icon"></v-icon>{{menu.name}}
           </a>
         </template>
@@ -100,6 +100,23 @@
           this.closeMenu(menu)
           this.openedMenu = null
         }
+      },
+      clearFocus() {
+        this.menus.forEach(menu => {
+          menu._focus = false
+          if (menu.children) {
+            menu.children.forEach(child => {
+              child._focus = false
+            })
+          }
+        })
+      },
+      clickItem(child, menu) {
+        this.clearFocus()
+        menu && (menu._focus = true)
+        child._focus = true
+        this.$forceUpdate()
+        this.$emit('click', child.value || child.url)
       }
     }
   }
