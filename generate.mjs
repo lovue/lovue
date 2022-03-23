8c723ea570bc602e3f42ba4f5c43e7f6166d81c0
loliconer <vuedev@163.com>
2022-03-24 00:02:19 +0800

    improve: add script to generate templates.

---
+++ generate.mjs
@@ -0,0 +1,43 @@
+import * as fs from 'fs'
+
+const name = process.argv[2]
+
+const componentTemplate = `<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const props = defineProps<{}>()
+const emit = defineEmits([])
+</script>
+
+<template>
+  <div class="lv-input">
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
+`
+
+const demoTemplate = `<script setup lang="ts">
+import ComponentDemo from './ComponentDemo.vue'
+</script>
+
+<template>
+  <ComponentDemo id="to${name}" title="Lv${name}">
+    <div class="usage">
+      <pre><code>// types
+const props = defineProps&lt;{}&gt;()
+const emit = defineEmits([])
+      </code></pre>
+    </div>
+
+    <div class="controls">
+      <Lv${name} />
+    </div>
+  </ComponentDemo>
+</template>
+`
+
+fs.writeFileSync(`src/components/Lv${name}.vue`, componentTemplate)
+fs.writeFileSync(`docs/Component${name}.vue`, demoTemplate)
